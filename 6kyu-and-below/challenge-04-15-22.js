c8dd347e82ea8b301c24f890bffff8e07ff77ace
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-15 21:22:15 -0400

    added more challenges for April 15 2022

--- 6kyu-and-below/challenge-04-15-22.js
+++ 6kyu-and-below/challenge-04-15-22.js
@@ -49,3 +49,38 @@ function twoSum(numbers, target) {
   })
   return final_indexes
 }
+
+/* The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+Examples
+
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+
+Notes
+
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input! */
+
+function duplicateEncode(word){
+    // ...
+  let final_str = ''
+  const str_array = word.toLowerCase().split('')
+  
+  str_array.forEach((letter,index) => {
+    let encoding = '('
+    for(let i = 0; i < str_array.length; i++){
+      if((i != index) && (letter == str_array[i])){
+        encoding = ')'
+      }  
+    }
+    final_str += encoding
+  })
+  return final_str
+}
+
+console.log(duplicateEncode("Success"))
+  //")())())"
+
+console.log(duplicateEncode("din"))
+  //(((
