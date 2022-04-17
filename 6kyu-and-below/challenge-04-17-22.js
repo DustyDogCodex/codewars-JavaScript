4690c4ea3d52331a1b7290e58a9bbca953b4f1d4
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-17 14:57:29 -0400

    new challenge

--- 6kyu-and-below/challenge-04-17-22.js
+++ 6kyu-and-below/challenge-04-17-22.js
@@ -20,3 +20,11 @@ function makeUpperCase(str) {
   // Code here
   return str.toUpperCase()
 }
+
+/* Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6] */
+
+const maps = x => x.map(num => num *2)
