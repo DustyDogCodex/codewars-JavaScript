b3ef0da246bbd064fd29de22b6d749b282ae3e11
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-17 17:32:09 -0400

    another one

--- 6kyu-and-below/challenge-04-17-22.js
+++ 6kyu-and-below/challenge-04-17-22.js
@@ -81,4 +81,32 @@ function SeriesSum(n){
   return series.toFixed(2)
 }
 
+/* Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+Examples
+
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  "" */
+
+function order(words){
+  // ...
+  let word_array = words.split(' ')
+  
+  let final_array = []
+  word_array.forEach(word => {
+    let letters = word.split('')
+    
+    letters.forEach(letter => {
+      if(Number(letter) != NaN){
+        final_array[Number(letter) - 1] = word 
+      }
+    })
+  })
+  return final_array.join(' ')
+}
+
 
