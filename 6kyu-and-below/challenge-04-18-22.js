9c19ac22de49cfa0e8d5cd1a62b41df4aa07946a
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-18 13:00:11 -0400

    added more challenges

--- 6kyu-and-below/challenge-04-18-22.js
+++ 6kyu-and-below/challenge-04-18-22.js
@@ -11,3 +11,22 @@ function opposite(number) {
   //your code here
   return -number
 }
+
+/* Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that! */
+
+function removeEveryOther(arr){
+  //your code here
+  let final_array = []
+  arr.forEach((word,index) => {
+    if(index % 2 == 0){
+      final_array.push(word)
+    }
+  })
+  return final_array
+}
