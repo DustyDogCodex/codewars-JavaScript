01dd9388d58f9ec55b9042e0d28cbee7600ca26a
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-20 23:48:12 -0400

    another one

--- 6kyu-and-below/challenge-04-20-22.js
+++ 6kyu-and-below/challenge-04-20-22.js
@@ -31,3 +31,30 @@ function getSum( a,b ){
   }
   return sum
 }
+
+/* Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+Example
+
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+ */
+
+function countPositivesSumNegatives(input) {
+  if(input == null || input.length == 0){
+    return []
+  }
+  
+  let pos = 0
+  let neg = 0 
+  input.forEach(num => {
+  if(num < 0){
+    neg += num
+  } else if (num > 0){
+    pos++
+  }
+  })
+  return [pos,neg]
+}
