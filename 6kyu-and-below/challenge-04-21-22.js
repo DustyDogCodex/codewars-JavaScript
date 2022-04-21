ee40594875c45ea59da7d6eee83ea8d2bad2345d
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-21 00:49:14 -0400

    another one. This one a 6 kyu!

--- 6kyu-and-below/challenge-04-21-22.js
+++ 6kyu-and-below/challenge-04-21-22.js
@@ -45,3 +45,38 @@ function expandedForm(num) {
   })
   return str
 }
+
+/* Count the number of Duplicates
+
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+Example
+
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice */
+
+function duplicateCount(text){
+  //...
+  let letter_array = text.toLowerCase().split('')
+  let checked_letters = []
+  let count = 0
+  letter_array.forEach((letter,index) => {
+    let duplicate = false
+    if(!checked_letters.includes(letter)){
+      for(let i = index+1; i < letter_array.length; i++){
+        if (letter_array[i] == letter){
+          duplicate = true
+          checked_letters.push(letter)
+        }
+      }
+    }
+    if (duplicate == true){
+      count++
+    }
+  })
+  return count
+}
