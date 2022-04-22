7a8f6383062cf4828a029aa8bacaec9488719d2d
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-21 23:55:02 -0400

    last one for today

--- 6kyu-and-below/challenge-04-21-22.js
+++ 6kyu-and-below/challenge-04-21-22.js
@@ -166,4 +166,35 @@ function sumDigPow(a, b) {
   return final_array
 }
 
+/* Task
+
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+Example
+
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+
+Input validation
+
+If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0. */
+
+function sumArray(array) {
+  if(array == null || array.length == 0 || array.length <=2){
+    return 0
+  }
+  const sorted_array = array.sort(function(a,b){return a-b;})
+  sorted_array.pop()
+  sorted_array.shift()
+  let sum = 0
+  sorted_array.forEach(num => {
+    sum += num
+  })
+  return sum
+}
+
+
 
