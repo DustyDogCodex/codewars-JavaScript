f49325a986fde5881ce21d32d2a76c036fda4276
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-21 22:59:52 -0400

    another challenge for today

--- 6kyu-and-below/challenge-04-21-22.js
+++ 6kyu-and-below/challenge-04-21-22.js
@@ -104,3 +104,29 @@ function finalGrade (exam, projects) {
   }
 }
 
+/* Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+
+The correct answer would be 17. */
+
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  if(arrayOfSheep.length == 0 || arrayOfSheep == null){
+    return 0
+  } else {
+    let count = 0
+    arrayOfSheep.forEach(sheep => {
+      if(sheep == true){ count++ }
+    })
+    return count
+  }
+}
+
