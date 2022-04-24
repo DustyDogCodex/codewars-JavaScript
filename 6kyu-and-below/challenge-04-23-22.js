d56d6913a93eec0b3d6ac2a3d4ae4d5f542e73f1
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-23 21:16:55 -0400

    another challenge for today

--- 6kyu-and-below/challenge-04-23-22.js
+++ 6kyu-and-below/challenge-04-23-22.js
@@ -34,5 +34,10 @@ function litres(time) {
   return Math.floor(time*0.5);
 }
 
+/* You are given two interior angles (in degrees) of a triangle.
 
+Write a function to return the 3rd. */
 
+function otherAngle(a, b) {
+  return (180-a-b);
+}
