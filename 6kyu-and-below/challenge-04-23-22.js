d60a798ce52d473788d6652acd3277c7f2e18012
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-23 21:59:28 -0400

    more challenges

--- 6kyu-and-below/challenge-04-23-22.js
+++ 6kyu-and-below/challenge-04-23-22.js
@@ -104,4 +104,15 @@ function removeExclamationMarks(s) {
   return s.replace(/!/g, '');
 }
 
+/* Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+ */
+
+function isDivideBy(number, a, b) {
+  // good luck
+  if(number%a == 0 && number%b ==0){
+    return true
+  } else {
+    return false
+  }
+}
 
