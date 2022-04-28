a16aae09bff677785658c5989e6a8698df6d57fe
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-28 19:56:51 -0400

    another one

--- 6kyu-and-below/challenge-04-28-22.js
+++ 6kyu-and-below/challenge-04-28-22.js
@@ -53,3 +53,14 @@ function findSum(n) {
   }
   return sum;
 }
+
+/* Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :) */
+
+function unusualFive() {
+  return 'Varun'.length
+}
+
