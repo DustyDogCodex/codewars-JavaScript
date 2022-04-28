ad2860367b28b8d4936c9b91e33499a4c8f16322
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-28 19:48:25 -0400

    another one

--- 6kyu-and-below/challenge-04-28-22.js
+++ 6kyu-and-below/challenge-04-28-22.js
@@ -22,3 +22,15 @@ function lovefunc(flower1, flower2){
   // moment of truth
   return (flower1 % 2 == 0 && flower2 % 2 != 0 || flower1 % 2 != 0 && flower2 % 2 == 0)
 }
+
+/* Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error". */
+
+function problem(x){
+  //your code here
+  if(typeof x == 'number'){
+    return x * 50 + 6
+  } else {
+    return 'Error'
+  }
+}
+
