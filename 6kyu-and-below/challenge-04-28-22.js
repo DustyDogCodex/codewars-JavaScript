b3c227f8ed03142565de92d5438f706ee10f193d
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-28 21:39:41 -0400

    another one

--- 6kyu-and-below/challenge-04-28-22.js
+++ 6kyu-and-below/challenge-04-28-22.js
@@ -135,6 +135,23 @@ function between(a, b) {
   return result
 }
 
+/* Description:
+
+Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+Examples
+
+replace("Hi!") === "H!!"
+replace("!Hi! Hi!") === "!H!! H!!"
+replace("aeiou") === "!!!!!"
+replace("ABCDE") === "!BCD!" */
+
+function replace(s){
+  //coding and coding....
+  return s.replace(/a|e|i|o|u/gi, '!')
+}
+
+
+
 
 
 
