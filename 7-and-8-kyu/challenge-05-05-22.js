77746bf8f0577e02f323cdcdbc761c2854470a7a
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-05 20:19:19 -0500

    final challenge for today

--- 7-and-8-kyu/challenge-05-05-22.js
+++ 7-and-8-kyu/challenge-05-05-22.js
@@ -44,3 +44,38 @@ function race(v1, v2, g) {
 function plural(n) {
   return n == 1 ? false : true
 }
+
+/* Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA" */
+
+function DNAStrand(dna){
+  const final_array = []
+  const letters_array = dna.split('')
+  letters_array.forEach(letter => {
+    switch(letter){
+        case 'A':
+          final_array.push('T')
+          break
+        case 'T':
+          final_array.push('A')
+          break 
+        case 'G':
+          final_array.push('C')
+          break
+        case 'C':
+          final_array.push('G')
+          break
+    }
+  })
+  return final_array.join('')
+}
