fdeb9cfb4c088a8d148960bd9a48399cfaedc9ae
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-05 20:19:19 -0500

    added another challenge today

--- 7-and-8-kyu/challenge-05-05-22.js
+++ 7-and-8-kyu/challenge-05-05-22.js
@@ -38,3 +38,9 @@ function race(v1, v2, g) {
   let time_array = [Number(hrs_array[0]),Number(mins_array[0]),Math.floor(secs)]
   return time_array
 }
+
+/* We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.  */
+
+function plural(n) {
+  return n == 1 ? false : true
+}
