c9a645e38710d894cca2070e049c3ad2e8bccdff
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-09 16:58:58 -0400

    added another challenge

--- 6kyu-and-below/challenge-05-09-22.js
+++ 6kyu-and-below/challenge-05-09-22.js
@@ -25,3 +25,22 @@ function gimme (triplet) {
   middle = temp_array[1]
   return triplet.indexOf(middle)
 }
+
+/* Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string */
+
+var number=function(array){
+  //your awesome code here
+  if(array.length == 0){
+    return []
+  }
+  const final_array = []
+  array.forEach((str,index) => {
+    let output = `${index+1}: ${str}`
+    final_array.push(output)
+  })
+  return final_array
+}
