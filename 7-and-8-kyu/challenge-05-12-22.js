0af7e60e05559d4ec4e10d2da60769bb9ddb636a
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-12 20:32:44 -0400

    another one - DJ Khaled

--- 7-and-8-kyu/challenge-05-12-22.js
+++ 7-and-8-kyu/challenge-05-12-22.js
@@ -44,3 +44,20 @@ function sumOfMinimums(arr) {
   })
   return sum
 }
+
+/* Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+Example
+
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] ); */
+
+var capitals = function(word) {
+	// Write your code here
+  let result = []
+  const letters = word.split('')
+  letters.forEach((letter,index) => {
+    if(letter.charCodeAt(0) > 64 && letter.charCodeAt(0) < 91){
+      result.push(index)
+    }
+  })
+  return result
+};
