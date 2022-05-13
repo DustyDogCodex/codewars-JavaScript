80f1f8abd63b6c5f6778016862bedece1e80bb7f
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-12 21:20:50 -0400

    another one

--- 7-and-8-kyu/challenge-05-12-22.js
+++ 7-and-8-kyu/challenge-05-12-22.js
@@ -61,3 +61,33 @@ var capitals = function(word) {
   })
   return result
 };
+
+/* Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+"hello world".toAlternatingCase() === "HELLO WORLD"
+"HELLO WORLD".toAlternatingCase() === "hello world"
+"hello WORLD".toAlternatingCase() === "HELLO world"
+"HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+"12345".toAlternatingCase() === "12345" // Non-alphabetical characters are unaffected
+"1a2b3c4d5e".toAlternatingCase() === "1A2B3C4D5E"
+"String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+
+As usual, your function/method should be pure, i.e. it should not mutate the original string.
+ */
+
+String.prototype.toAlternatingCase = function () {
+  // Define your method here :
+  let str = this.toString()
+  const letter_array = str.split('')
+  const result = []
+  letter_array.forEach((letter,index) => {
+    if(letter.charCodeAt(0) > 64 && letter.charCodeAt(0) < 91){
+      result.push(letter.toLowerCase())
+    } else if(letter.charCodeAt(0) > 96 && letter.charCodeAt(0) < 123){
+      result.push(letter.toUpperCase())
+    } else {
+      result.push(letter)
+    }
+  })
+  return result.join('')
+}
