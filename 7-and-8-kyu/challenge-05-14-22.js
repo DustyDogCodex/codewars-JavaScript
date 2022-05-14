c6dd8c6f334ac3a0e66dbf436eab590934c4c56b
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-14 18:03:43 -0400

    added more

--- 7-and-8-kyu/challenge-05-14-22.js
+++ 7-and-8-kyu/challenge-05-14-22.js
@@ -43,3 +43,22 @@ Examples (Input -> Output)
 function reverseList(list) {
   return list.reverse()
 }
+
+/* What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+addLength('apple ban') => ["apple 5", "ban 3"]
+addLength('you will win') => ["you 3", "will 4", "win 3"]
+
+Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+
+Note: String will have at least one element; words will always be separated by a space. */
+
+function addLength(str) {
+//start-here
+  const words = str.split(' ')
+  const final = []
+  words.forEach(word => {
+    final.push(word + ' ' + word.length)
+  })
+  return final
+}
