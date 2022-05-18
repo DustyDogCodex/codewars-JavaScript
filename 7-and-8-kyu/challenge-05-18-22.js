1d74d6be66d78c74adbc0087bc7886c9cebb772d
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-18 15:02:57 -0400

    plus one challenge

--- 7-and-8-kyu/challenge-05-18-22.js
+++ 7-and-8-kyu/challenge-05-18-22.js
@@ -29,4 +29,15 @@ function getEvenNumbers(numbersArray){
   return numbersArray.filter(num => num % 2 == 0)
 }
 
+/* Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times. */
+
+function repeater(string, n){
+  //Your code goes here.
+  let str = ''
+  for (let i = 0; i <n; i++){
+    str += string
+  }
+  return str
+}
+
 
