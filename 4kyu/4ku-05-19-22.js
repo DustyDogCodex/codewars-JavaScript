210eb8904c931220b23aa862bf7c99e74a5a644d
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-19 20:55:01 -0400

    refactored the code from the last challenge

--- 4kyu/4ku-05-19-22.js
+++ 4kyu/4ku-05-19-22.js
@@ -91,17 +91,19 @@ array = [[1,2,3],
          [7,6,5]]
 snail(array) #=> [1,2,3,4,5,6,7,8,9] */
 
-const snail = (array) =>{
-  let finalArray = []
-  while(array.length){
-    finalArray.push(...array.shift())
-    for (var i = 0; i < array.length; i++){
-      finalArray.push(array[i].pop())
-    }
-    finalArray.push(...(array.pop() || []).reverse())
-    for (var i = array.length -1; i >= 0; i--){
-      finalArray.push(array[i].shift())
-    }
+snail = function(array) {
+  var result;
+  while (array.length) {
+    // Steal the first row.
+    result = (result ? result.concat(array.shift()) : array.shift());
+    // Steal the right items.
+    for (var i = 0; i < array.length; i++)
+      result.push(array[i].pop());
+    // Steal the bottom row.
+    result = result.concat((array.pop() || []).reverse());
+    // Steal the left items.
+    for (var i = array.length - 1; i >= 0; i--)
+      result.push(array[i].shift());
   }
-  return finalArray
+  return result;
 }
