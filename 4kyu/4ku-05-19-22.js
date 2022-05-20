6b485c085c11a897846fec4e66fb6d61771bd0b1
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-19 20:53:06 -0400

    finished another 4kyu challenge today sheeeesh

--- 4kyu/4ku-05-19-22.js
+++ 4kyu/4ku-05-19-22.js
@@ -74,3 +74,34 @@ const mix = (s1, s2) => {
   return sortedResult.map((v) => `${v[1].greater}:${v[0].repeat(v[1].max)}`)
     .join('/');
 };
+
+/* Snail Sort
+
+Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]
+
+For better understanding, please follow the numbers of the next array consecutively:
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+snail(array) #=> [1,2,3,4,5,6,7,8,9] */
+
+const snail = (array) =>{
+  let finalArray = []
+  while(array.length){
+    finalArray.push(...array.shift())
+    for (var i = 0; i < array.length; i++){
+      finalArray.push(array[i].pop())
+    }
+    finalArray.push(...(array.pop() || []).reverse())
+    for (var i = array.length -1; i >= 0; i--){
+      finalArray.push(array[i].shift())
+    }
+  }
+  return finalArray
+}
