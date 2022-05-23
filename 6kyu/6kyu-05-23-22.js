015c9674f756fd9bce224f7bba058f0da49034a3
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-23 15:37:34 -0400

    solved another 6kyu today lets gooooooooo!

--- 6kyu/6kyu-05-23-22.js
+++ 6kyu/6kyu-05-23-22.js
@@ -19,3 +19,51 @@ function isValidWalk(walk) {
     }
     return north_south == 0 && east_west == 0; 
   }
+
+/* Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+Task
+
+You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+Examples
+
+A size 3 diamond:
+
+ *
+***
+ *
+
+...which would appear as a string of " *\n***\n *\n"
+
+A size 5 diamond:
+
+  *
+ ***
+*****
+ ***
+  *
+
+...that is:
+
+"  *\n ***\n*****\n ***\n  *\n" */
+
+function diamond(n){
+  if (n <= 0 || n % 2 === 0) {
+    return null;
+  }
+
+  const repeat = ch => n => ch.repeat(n)
+  const space = repeat(' ')
+  const asterix = repeat('*')
+
+  let diamond_row = ''
+  for (let row = 1; row <= n; row++) {
+    const spaces = Math.abs(n - ((2*row) - 1))
+    const stars = n - spaces
+    diamond_row += `${space(spaces / 2)}${asterix(stars)}\n`
+  }
+
+  return diamond_row
+}
+
