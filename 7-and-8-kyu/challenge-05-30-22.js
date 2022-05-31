7f613f6edf54e49f303ab76c5323f335c47c33da
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-30 20:19:19 -0500

    added another challenge for today

--- 7-and-8-kyu/challenge-05-30-22.js
+++ 7-and-8-kyu/challenge-05-30-22.js
@@ -9,3 +9,19 @@ function solution(str){
   const str_array = str.split('')
   return str_array.reverse().join('')
 }
+
+/* An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+Examples
+
+    "foefet" is an anagram of "toffee"
+
+    "Buckethead" is an anagram of "DeathCubeK" */
+
+var isAnagram = function(test, original) {
+ return (test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join(''));
+};
+
