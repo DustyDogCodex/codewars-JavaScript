8d7227f9af8438bc72ca1e6443d4c1ecc5af4259
Varun Malaviya <varunmalaviya7@gmail.com>
2022-05-30 20:19:19 -0500

    added another challenge

--- 7-and-8-kyu/challenge-05-30-22.js
+++ 7-and-8-kyu/challenge-05-30-22.js
@@ -35,3 +35,28 @@ var b = "Lab"
 
 var name = a + b
 
+/* Description:
+
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+Examples
+
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+ */
+
+const remove = function(str, n) {
+  let i = 0;
+  const res = str.replace(/!/g, match => i++ < n ? '' : match);
+  return res;
+}
+
+
