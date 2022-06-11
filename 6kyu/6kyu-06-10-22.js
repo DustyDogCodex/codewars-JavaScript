f771f65380f1ceabaa55d8acf79115940313f95a
Varun Malaviya <varunmalaviya7@gmail.com>
2022-06-10 20:19:19 -0500

    another completed challenge for today

--- 6kyu/6kyu-06-10-22.js
+++ 6kyu/6kyu-06-10-22.js
@@ -34,3 +34,10 @@ function queueTime(customers, n) {
   }
   return Math.max(...w);
 }
+
+/* Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array. */
+
+function take(arr, n) {
+  // Your code here
+  return arr.slice(0,n)
+}
