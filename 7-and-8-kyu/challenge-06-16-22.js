9dbe68492493b26f137c2f95fc2692db8825a7cd
Varun Malaviya <varunmalaviya7@gmail.com>
2022-06-16 09:33:08 -0400

    added another challenge

--- 7-and-8-kyu/challenge-06-16-22.js
+++ 7-and-8-kyu/challenge-06-16-22.js
@@ -15,3 +15,8 @@ function sayHello( name, city, state ) {
   return `Hello, ${name_str}! Welcome to ${city}, ${state}!`
 }
 
+/* Complete the function which converts hex number (given as a string) to a decimal number. */
+
+function hexToDec(hexString){
+  return parseInt(hexString,16)
+}
