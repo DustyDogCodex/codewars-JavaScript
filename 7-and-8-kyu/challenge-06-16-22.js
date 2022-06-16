df80a2749648cd15fac0639f5b42a8d561a7cf1a
Varun Malaviya <varunmalaviya7@gmail.com>
2022-06-16 09:36:03 -0400

    added another challenge for today

--- 7-and-8-kyu/challenge-06-16-22.js
+++ 7-and-8-kyu/challenge-06-16-22.js
@@ -20,3 +20,13 @@ function sayHello( name, city, state ) {
 function hexToDec(hexString){
   return parseInt(hexString,16)
 }
+
+/* *** No Loops Allowed ***
+
+You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case. */
+
+function check(a,x){
+  return a.some(num => num == x)
+}
