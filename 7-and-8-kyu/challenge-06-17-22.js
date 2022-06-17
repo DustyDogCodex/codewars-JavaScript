611b8005eeaa4824b226e35018e703061fd7037d
Varun Malaviya <varunmalaviya7@gmail.com>
2022-06-17 11:06:15 -0400

    added another one

--- 7-and-8-kyu/challenge-06-17-22.js
+++ 7-and-8-kyu/challenge-06-17-22.js
@@ -10,3 +10,30 @@ If the sperm contains the X chromosome, return "Congratulations! You're going to
 function chromosomeCheck(sperm) {
   return sperm.includes("Y") ?  "Congratulations! You're going to have a son." : "Congratulations! You're going to have a daughter."
 }
+
+/* We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+So given a string "super", we should return a list of [2, 4].
+
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+
+NOTES
+
+    Vowels in this context refers to: a e i o u y (including upper case)
+    This is indexed from [1..n] (not zero indexed!) */
+
+function vowelIndices(word){
+  //your code here
+  const vowels = ["a","e",'i','o','u','y']
+  const letters_array = word.toLowerCase().split('')
+  const result = []
+  letters_array.forEach((letter,index) => {
+    vowels.includes(letter) ? result.push(index+1) : ''
+  })
+  return result
+}
+
