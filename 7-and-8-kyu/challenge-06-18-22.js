7bab8890689e12795f3e42f85e9c9f3b88140612
Varun Malaviya <varunmalaviya7@gmail.com>
2022-06-18 17:30:51 -0400

    added another one

--- 7-and-8-kyu/challenge-06-18-22.js
+++ 7-and-8-kyu/challenge-06-18-22.js
@@ -17,3 +17,33 @@ function evaporator(content, evap_per_day, threshold){
   }
   return counter
 }
+
+/* Complete function howManydays, function accept 1 parameters:month, means the month of year, different month has different days (refer to the following table), return a number that how many days in this month(month is always greater than 0, less than or equal to 12).
+
++---------------+-------------+
+|    month      |    days     |
++---------------+-------------+
+|1,3,5,7,8,10,12|     31      |
++---------------+-------------+
+|4,6,9,11       |     30      |
++---------------+-------------+
+|2              |     28      |  (Do not consider the leap year)
++---------------+-------------+
+ */
+
+function howManydays(month){
+  switch (month){
+      case 2:
+        return 28
+      case 4:
+        return 30
+      case 6:
+        return 30
+      case 9:
+        return 30
+      case 11:
+        return 30
+      default:
+        return 31
+  }
+}
