f3f42bc9386fc311ed4a136a1a9f91f267d188c1
Varun Malaviya <varunmalaviya7@gmail.com>
2022-06-21 11:24:03 -0400

    added another challenge fpr today

--- 7-and-8-kyu/challenge-06-21-22.js
+++ 7-and-8-kyu/challenge-06-21-22.js
@@ -26,3 +26,34 @@ In the end, click "Submit" to submit your code pass this kata. */
 function animal(obj){
   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
 }
+
+/* Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+    if x > y: 3 points
+    if x < y: 0 point
+    if x = y: 1 point
+
+Notes:
+
+    there are 10 matches in the championship
+    0 <= x <= 4
+    0 <= y <= 4
+ */
+
+function points(games) {
+  // your code here
+  let points_tally = 0
+  games.forEach(game => {
+    const match_results = game.split(':')
+    if(match_results[0] > match_results[1]){
+      points_tally += 3
+    } else if (match_results[0] == match_results[1]){
+      points_tally += 1
+    }
+  })
+  return points_tally
+}
