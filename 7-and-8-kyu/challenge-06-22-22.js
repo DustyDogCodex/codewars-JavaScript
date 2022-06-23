354ad7acb378f84aa93dcfeb725569446395c01e
Varun Malaviya <varunmalaviya7@gmail.com>
2022-06-22 22:22:25 -0400

    another challenge for today

--- 7-and-8-kyu/challenge-06-22-22.js
+++ 7-and-8-kyu/challenge-06-22-22.js
@@ -17,3 +17,22 @@ var cubeChecker = function(volume, side){
   if(side <= 0) {return false}
   return side ** 3 == volume;
 };
+
+/* Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+
+Assume that the input n will always be a positive integer.
+
+Examples: (Input --> output)
+
+2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27) */
+
+function sumCubes(n){
+  // ...
+  let sum = 0
+  for(let i = 0; i <= n; i++){
+    sum += i ** 3
+  }
+  return sum
+}
+
