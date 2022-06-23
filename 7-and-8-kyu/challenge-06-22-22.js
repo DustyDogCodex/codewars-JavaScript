9b39ee598bb9f5617d1ac134e9558f060409a196
Varun Malaviya <varunmalaviya7@gmail.com>
2022-06-22 22:31:32 -0400

    last challenge for today

--- 7-and-8-kyu/challenge-06-22-22.js
+++ 7-and-8-kyu/challenge-06-22-22.js
@@ -36,3 +36,22 @@ function sumCubes(n){
   return sum
 }
 
+/* I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+Example
+
+n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++ */
+
+function generateShape(integer){
+  let result = ''
+  for(let i = 0; i < integer; i++){
+    i < integer - 1 ? result += `${'+'.repeat(integer)}\n` : result += `${'+'.repeat(integer)}` 
+  }
+  return result
+}
+
+
+
