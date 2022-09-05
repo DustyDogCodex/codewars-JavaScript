8cb767901e8a8a0351d99eb4fb0617ccda770c1b
Varun Malaviya <varunmalaviya7@gmail.com>
2022-09-04 20:19:19 -0500

    last challenge for today

--- 7-and-8-kyu/challenge-09-04-22.js
+++ 7-and-8-kyu/challenge-09-04-22.js
@@ -31,3 +31,17 @@ pointsPer48(0, 0) // 0
 function pointsPer48(ppg, mpg) {
   return mpg == 0 ? 0 : Number(((ppg/mpg) * 48).toFixed(1))
 }
+
+/* You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+    lst contains integers, that means it may contain some negative numbers
+    if lst is empty or contains a single element, return 0
+    lst is not sorted
+
+[1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+[1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+ */
+
+function maxDiff(list) {
+  return list.length < 2 ? 0 : Math.max(...list) - Math.min(...list);
+};
