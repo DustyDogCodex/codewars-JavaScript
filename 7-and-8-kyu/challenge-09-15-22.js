3d72a95ef4a8f8c01d3310a9a185e7db050f5f2a
Varun Malaviya <varunmalaviya7@gmail.com>
2022-09-15 19:20:07 -0400

    todays challenges

---
+++ 7-and-8-kyu/challenge-09-15-22.js
@@ -0,0 +1,24 @@
+/* Rock Paper Scissors
+
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+ */
+
+const rps = (p1, p2) => {
+  const map = {
+    'rock': 'scissors',
+    'scissors': 'paper',
+    'paper': 'rock'
+  };
+  
+  if (p1 == p2) {
+    return 'Draw!';
+  } else {
+    return 'Player ' + (map[p1] == p2 ? 1 : 2) + ' won!';
+  }
+};
