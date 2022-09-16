79082d36309d3efaea6e66a4d68e5ddca3e1f0a4
Varun Malaviya <varunmalaviya7@gmail.com>
2022-09-16 17:18:35 -0400

    last challenge for today

--- 7-and-8-kyu/challenge-09-16-22.js
+++ 7-and-8-kyu/challenge-09-16-22.js
@@ -63,3 +63,21 @@ function predictAge(...args){
   return Math.floor(Math.sqrt(sum)/2)
 }
 
+/* In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C). */
+
+function factorial(n)
+{
+  if (n < 0 || n > 12){
+    throw new RangeError('RangeError')
+  } 
+  if(n == 0){
+    return 1
+  }
+  let factorial = 1
+  for(let i = n; i >= 1; i--){
+    factorial *= i
+  }
+  return factorial
+}
