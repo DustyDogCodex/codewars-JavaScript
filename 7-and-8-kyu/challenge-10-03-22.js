13d939c670aad8a40b16f4768547ab18adb110ca
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-03 14:50:25 -0400

    last challenge for today

--- 7-and-8-kyu/challenge-10-03-22.js
+++ 7-and-8-kyu/challenge-10-03-22.js
@@ -30,3 +30,11 @@ first(arr, 0) //=> [];
 function first(arr, n = 1) { 
   return n == 0 ? [] : arr.slice(0,n)
 }
+
+/* Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number(). */
+
+function add(a, b){
+	return a == b
+}
