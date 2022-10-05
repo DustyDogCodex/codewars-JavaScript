379c7634a1fdd0ec34f1f3cece1cffbf82afb321
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-05 15:23:01 -0400

    another challenge for today

--- 7-and-8-kyu/challenge-10-05-22.js
+++ 7-and-8-kyu/challenge-10-05-22.js
@@ -36,5 +36,28 @@ function answer3(){
   return "yes";
 }
 
+/* Task
 
+Coding in function padIt, function accept 2 parameters:
+
+    str, it's a string representing the string to pad, we need pad some "*" at leftside or rightside of str
+    n, it's a number, how many times to pad the string.
+
+Behaviour
+
+You need to write a loop statement within the function that loops n times. Each time through the loop it will add one * to str, alternating on which side it is padded: the first time will add a * to the left side of str, the second time will add a * to the right side, and so on.
+
+Finally, return the padded string. */
+
+function padIt(str,n){
+  //coding here
+  let i = 0
+  let left_side = ''
+  let right_side = ''
+  while(i < n){
+    i % 2 == 0 ? left_side += "*" : right_side += "*"
+    i++
+  }
+  return left_side+str+right_side
+}
 
