f8d7d905777f63a921a441fc3346a2eaf4d33175
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-04 20:19:19 -0500

    last challenge for today

--- 7-and-8-kyu/challenge-10-04-22.js
+++ 7-and-8-kyu/challenge-10-04-22.js
@@ -59,3 +59,46 @@ function calculateYears(principal, interest, tax, desired) {
     }
     return years;
 }
+
+/* Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+
+You need to consider the following ratings:
+
+    Terrible: tip 0%
+    Poor: tip 5%
+    Good: tip 10%
+    Great: tip 15%
+    Excellent: tip 20%
+
+The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+
+    "Rating not recognised" in Javascript, Python and Ruby...
+    ...or null in Java
+    ...or -1 in C#
+
+Because you're a nice person, you always round up the tip, regardless of the service. */
+
+function calculateTip(amount, rating) {
+  let tip
+  switch(rating.toLowerCase()){
+      case 'terrible':
+        tip = 0
+        break
+      case 'poor':
+        tip = 0.05
+        break
+      case 'good':
+        tip = 0.10
+        break
+      case 'great':
+        tip = 0.15
+        break
+      case 'excellent':
+        tip = 0.20
+        break  
+      default:
+        return "Rating not recognised"
+  }
+  return Math.ceil(amount * tip) 
+}
+
