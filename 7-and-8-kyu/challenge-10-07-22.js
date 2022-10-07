fba2c3cca9bd2bccb186ede8b8cb998881e258af
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-07 17:26:07 -0400

    last challenge for today

--- 7-and-8-kyu/challenge-10-07-22.js
+++ 7-and-8-kyu/challenge-10-07-22.js
@@ -44,4 +44,17 @@ function correctPolishLetters (string) {
     return string.split('').map((c) => polishLetters[c] || c).join("");
 }
 
+/* Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid. */
+
+function last(x){
+
+  return x.split(' ').sort((a,b) => a.charCodeAt(a.length-1) - b.charCodeAt(b.length-1))
+
+}
+
+
 
