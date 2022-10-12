5d1051ebd348ab789e8c8d8ce7aa468cabab69c8
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-12 11:10:26 -0400

    added anothher 6kyu

--- 6kyu/6kyu-10-12-22.js
+++ 6kyu/6kyu-10-12-22.js
@@ -14,3 +14,16 @@ function spinWords(string){
   })
   return reversed_array.join(' ')
 }
+
+/* Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case */
+
+var countBits = function(n) {
+  const binary_array = n.toString(2).split('')
+  let count = 0
+  binary_array.forEach(num => {
+    +num == 1 ? count++ : ''  
+  })  
+  return count
+};
