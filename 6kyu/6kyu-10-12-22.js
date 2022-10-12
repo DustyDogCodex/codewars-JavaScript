3c63ce3a532057f524f89e7e179c8e754233c335
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-12 11:22:22 -0400

    last 6kyu for today

--- 6kyu/6kyu-10-12-22.js
+++ 6kyu/6kyu-10-12-22.js
@@ -27,3 +27,30 @@ var countBits = function(n) {
   })  
   return count
 };
+
+/* You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+Examples
+
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number) */
+
+function findOutlier(integers){
+  //your code here
+  let even_count = 0
+  let odd_count = 0
+  for (let i = 0; i <= 2; i++){
+    integers[i] % 2 == 0 ? even_count++ : odd_count++
+  }
+  if (even_count > odd_count){
+    const result = integers.filter(num => num % 2 != 0)
+    return +result[0]
+  } else {
+    const result = integers.filter(num => num % 2 == 0)
+    return +result[0]
+  }
+}
+
+
