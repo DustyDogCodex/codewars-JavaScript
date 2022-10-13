674ecf0d55e2ed816a10c5eb85359ff81a947bd6
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-13 12:26:00 -0400

    last challenge for today

--- 6kyu/6kyu-10-13-22.js
+++ 6kyu/6kyu-10-13-22.js
@@ -48,3 +48,18 @@ function narcissistic(value) {
   })
   return value == sum
 }
+
+/* There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance. */
+
+function findUniq(arr) {
+  // do magic
+  const sorted_array = arr.sort((a,b) => a - b)
+  return sorted_array[0] == sorted_array[1] ? sorted_array[sorted_array.length-1] : sorted_array[0]
+}
