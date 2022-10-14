d65e31e0f77cffc63b362fa66388767e84bc42ce
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-14 14:14:27 -0400

    challnenges for today

---
+++ 6kyu/6kyu-10-14-22.js
@@ -0,0 +1,20 @@
+/* Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+
+ */
+
+function solution(str){
+  const result = []
+  if(str == ''){
+    return []
+  }
+  str.length % 2 == 0 ? '' : str += '_'
+  for(let i = 0; i < str.length; i += 2){
+    result.push(str.slice(i,i+2))
+  }
+  return result
+}
