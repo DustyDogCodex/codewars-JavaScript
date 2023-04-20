4dcee9b1dac6848e9c89241cd834cd31937cef32
Varun Malaviya <varunmalaviya7@gmail.com>
2023-04-20 14:43:02 -0400

    second challenge for today

--- 7-and-8-kyu/challenge-04-20-23.js
+++ 7-and-8-kyu/challenge-04-20-23.js
@@ -18,3 +18,85 @@ function multiples(m, n){
   }
   return final_array
 }
+
+/* Definition
+
+Jumping number is the number that All adjacent digits in it differ by 1.
+Task
+
+Given a number, Find if it is Jumping or not.
+Notes
+
+    Number passed is always Positive .
+
+    Return the result as String .
+
+    The difference between ‘9’ and ‘0’ is not considered as 1 .
+
+    All single digit numbers are considered as Jumping numbers.
+
+Input >> Output Examples
+
+jumpingNumber(9) ==> return "Jumping!!"
+
+Explanation:
+
+    Adjacent digits differ by 1
+
+jumpingNumber(556847) ==> return "Not!!"
+
+Explanation:
+
+    Adjacent digits don't differ by 1
+
+jumpingNumber(4343456) ==> return "Jumping!!"
+
+Explanation:
+
+    Adjacent digits differ by 1
+ */
+
+function jumpingNumber(n){
+  //your code here
+  let str = n.toString()
+  let len = str.length
+  let two_digit_num = false
+  const str_array = str.split('')
+  
+  if ( len == 1) {
+    return "Jumping!!"
+  } else if (len == 2) {
+    
+      if (Number(str_array[0]) == (Number(str_array[1]) + 1) || Number(str_array[0]) == (Number(str_array[1]) - 1)){
+        two_digit_num = true
+      }
+      
+      return two_digit_num ? "Jumping!!" : "Not!!"
+    
+  } else if (len >= 3) {
+  
+      let first_digit = false
+      let last_digit = false
+      let mid_digits = false
+      
+      if (Number(str_array[0]) == (Number(str_array[1]) + 1) || Number(str_array[0]) == (Number(str_array[1]) - 1)){
+        first_digit = true
+      }
+      
+      if (Number(str_array[len-1]) == (Number(str_array[len-2]) + 1) || Number(str_array[len-1]) == (Number(str_array[len-2]) - 1)){
+        last_digit = true
+      } 
+      
+      str_array.forEach((num,index) => {
+        if (index >= 1 && index <= (len - 2)){
+          if(num == (str_array[index-1] - 1) || num == (str_array[index-1] + 1) || (num == (str_array[index+1] - 1) || num == (str_array[index+1] + 1))){
+            mid_digits = true
+          } else {
+            mid_digits = false
+          }   
+        }
+      })
+      
+    return (first_digit && last_digit && mid_digits) ? "Jumping!!" : "Not!!"
+    }
+}
