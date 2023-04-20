dcdb87fce8ad580d63439d9b455880f769798078
Varun Malaviya <varunmalaviya7@gmail.com>
2023-04-20 14:54:34 -0400

    last challenge for today

--- 7-and-8-kyu/challenge-04-20-23.js
+++ 7-and-8-kyu/challenge-04-20-23.js
@@ -100,3 +100,32 @@ function jumpingNumber(n){
     return (first_digit && last_digit && mid_digits) ? "Jumping!!" : "Not!!"
     }
 }
+
+/* Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+Example:
+
+For input: [3, 4, 4, 3, 6, 3]
+
+    remove the 3 at index 0
+    remove the 4 at index 1
+    remove the 3 at index 3
+
+Expected output: [4, 6, 3]
+
+More examples can be found in the test cases.
+
+Good luck! */
+
+function solve(arr) {
+  const final_array = []
+  const rev_arr = arr.reverse()
+  rev_arr.forEach(num => {
+    if (final_array.some(el => el == num)){
+      ''
+    } else {
+      final_array.push(num)
+    }
+  })
+  
+  return final_array;
+}
