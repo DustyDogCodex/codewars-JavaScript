e075ea765c0a836f79ad992a54fab0927feba63c
Varun Malaviya <varunmalaviya7@gmail.com>
2023-04-19 21:18:43 -0400

    second challenge for today

--- 7-and-8-kyu/challenge-04-19-23.js
+++ 7-and-8-kyu/challenge-04-19-23.js
@@ -32,3 +32,35 @@ function grader(score) {
   } 
   return result
 }
+
+/* Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+Your task is to create a function that, given a proper first and last name, will return the correct alias.
+Notes:
+
+    Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given. See the examples below for further details.
+
+    If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+    Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+Examples
+
+// These two objects are preloaded, you need to use them in your code
+var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+
+aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.' */
+
+function aliasGen(name, lastName){
+    // Code Here
+  const first_name = name.slice(0,1).toUpperCase()
+  const first_lastname = lastName.slice(0,1).toUpperCase()
+  
+  if((first_name.charCodeAt(0) < 65 || first_name.charCodeAt(0) > 90) || (first_lastname.charCodeAt(0) < 65 || first_lastname.charCodeAt(0) > 90)){
+    return "Your name must start with a letter from A - Z."
+  } else {
+    return `${firstName[first_name]} ${surname[first_lastname]}`
+  }
+}
