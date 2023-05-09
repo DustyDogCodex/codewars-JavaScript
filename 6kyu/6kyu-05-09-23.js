1cde52aa67532b411d913b87e408b3924e15f464
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-09 13:10:32 -0400

    second 6kyu for today

--- 6kyu/6kyu-05-09-23.js
+++ 6kyu/6kyu-05-09-23.js
@@ -28,3 +28,33 @@ multiplicationTable = function(size) {
   }
   return result
 }
+
+/* Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+Your task is to process a string with "#" symbols.
+Examples
+
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+
+ */
+
+function cleanString(s) {
+  if(s == ''){
+    return ''
+  }
+  
+  const result = [];
+
+  for (const letter of s) {
+        if (letter !== "#") {
+            result.push(letter);
+        } else if (result.length) {
+            result.pop();
+        }
+    }
+
+    return result.join('');
+}
