d84933661dcc0d9eafb656d46c25925883c6436b
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-18 20:54:08 -0400

    second 6kyu for today

--- 6kyu/6kyu-05-18-23.js
+++ 6kyu/6kyu-05-18-23.js
@@ -16,3 +16,18 @@ function getParticipants(handshakes){
     } 
     return count-1
 }
+
+/* The test fixture I use for this kata is pre-populated.
+
+It will compare your guess to a random number generated using:
+
+Math.floor(Math.random() * 100 + 1)
+
+You can pass by relying on luck or skill but try not to rely on luck.
+
+"The power to define the situation is the ultimate power." - Jerry Rubin
+
+Good luck! */
+
+Math.random = function(n){return 0;}
+var guess = 1;
